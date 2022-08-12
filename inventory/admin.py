67c2bcdc04af8af826ebe5c7b79c8b976f8f80bb
Ruslan Forostianov <ruslan@forostianov.com>
2022-08-12 18:00:06 +0200

    Register all model objects with admin site

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Building, Spot, Person, ArtItem, ArtLocation 
+
+admin.site.register(Building)
+admin.site.register(Spot)
+admin.site.register(Person)
+admin.site.register(ArtItem)
+admin.site.register(ArtLocation)
