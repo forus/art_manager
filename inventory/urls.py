2d6e10ebbe1e9558321b1722382076a794939004
Ruslan Forostianov <ruslan@forostianov.com>
2022-08-18 23:43:06 +0200

    Introduce inventory api with building and spots endpoints

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,30 @@
-from django.urls import path
+from django.urls import include, path
 
-from . import views
+from .models import Building, Spot
+from rest_framework import routers, serializers, viewsets
+
+class BuildingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Building
+        fields = ['address']
+
+class SpotSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Spot
+        fields = ['building', 'room', 'floor', 'details']
+
+class BuildingViewSet(viewsets.ModelViewSet):
+    queryset = Building.objects.all()
+    serializer_class = BuildingSerializer
+
+class SpotViewSet(viewsets.ModelViewSet):
+    queryset = Spot.objects.all()
+    serializer_class = SpotSerializer
+
+router = routers.DefaultRouter()
+router.register(r'buildings', BuildingViewSet)
+router.register(r'spots', SpotViewSet)
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', include(router.urls)),
 ]
