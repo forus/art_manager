7b3532046bb9e7119fdb2a9a199603343bce81ff
Ruslan Forostianov <ruslan@forostianov.com>
2022-09-26 21:37:40 +0200

    DeSerialize user first and last name

--- inventory/api/serializers/user.py
+++ inventory/api/serializers/user.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'is_staff']
+        fields = ['url', 'username', 'first_name', 'last_name', 'email', 'is_staff']
