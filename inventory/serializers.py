1c299a4bacb9390e0032a85f02ae6ced33404cec
Ruslan Forostianov <ruslan@forostianov.com>
2022-09-26 11:49:46 +0200

    Move viewsets and serializers from urls to their own module

--- inventory/serializers.py
+++ inventory/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import *
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
         fields = ['url', 'username', 'email', 'is_staff']
+    
+class BuildingSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Building
+        fields = ['address']
+
+class SpotSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Spot
+        fields = ['building', 'room', 'floor', 'details']
--- inventory/urls.py
+++ inventory/urls.py
@@ -1,27 +1,7 @@
 from django.urls import include, path
 
-from inventory.viewsets import UserViewSet
-
-from .models import Building, Spot
-from rest_framework import routers, serializers, viewsets
-
-class BuildingSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Building
-        fields = ['address']
-
-class SpotSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Spot
-        fields = ['building', 'room', 'floor', 'details']
-
-class BuildingViewSet(viewsets.ModelViewSet):
-    queryset = Building.objects.all()
-    serializer_class = BuildingSerializer
-
-class SpotViewSet(viewsets.ModelViewSet):
-    queryset = Spot.objects.all()
-    serializer_class = SpotSerializer
+from inventory.viewsets import *
+from rest_framework import routers 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
