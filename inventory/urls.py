af8c685d7c0c7505e4c90f1c3c053c7e9f99e319
Ruslan Forostianov <ruslan@forostianov.com>
2022-10-13 06:05:40 +0200

    Set up automatic schema generation
    See docs here https://www.django-rest-framework.org/api-guide/schemas/

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,14 @@
 import imp
+from rest_framework.schemas import get_schema_view
 from django.urls import include, path
+
 from .api import router
 
 urlpatterns = [
     path('api/', include(router.urls)),
+    path('openapi/', get_schema_view(
+        title="Art Manager",
+        description="API of Art Manager project",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
