4cbb85722bf3bb557afcd094e7efa9e79407e23a
Ruslan Forostianov <ruslan@forostianov.com>
2022-10-13 06:15:54 +0200

    Set up swagger documentation page
    Seed docs here https://www.django-rest-framework.org/topics/documenting-your-api/

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,5 @@
-import imp
 from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 from django.urls import include, path
 
 from .api import router
@@ -11,4 +11,8 @@ urlpatterns = [
         description="API of Art Manager project",
         version="1.0.0"
     ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 ]
