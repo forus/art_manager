cd1722db99446aaab17dd3581c2002d3867b44bf
Ruslan Forostianov <ruslan@forostianov.com>
2022-10-13 06:26:11 +0200

    Example on how to add endpoint description to swagger

--- inventory/api/viewsets/users.py
+++ inventory/api/viewsets/users.py
@@ -3,5 +3,14 @@ from django.contrib.auth.models import User
 from inventory.api.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
+    """
+    Enpoint to create/read/update/delete a user enity
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        """
+        Enpoint to create a user enity
+        """
+        super().create(request, *args, **kwargs)
